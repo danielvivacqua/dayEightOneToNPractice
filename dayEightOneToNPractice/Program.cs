71ba446b9a333a1b5a324e2c12e0344695927096
Your Name <you@example.com>
2016-05-16 12:38:15 -0400

    Counting one to 'n'

---
+++ dayEightOneToNPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dayEightOneToNPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value for 'n' greater than 1");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int counter = 1; n >= counter; counter++)
+            {
+                if ((counter % 3) == 0 && (counter % 7 == 0))
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(counter);
+                }
+            }
+        }
+    }
+}
